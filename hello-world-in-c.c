baa46e73bcaae095965f18a2501a759ca783587f
Matthew Price <91096895+Prrice@users.noreply.github.com>
2022-09-20 02:56:26 -0700

    Add files via upload

---
+++ hello-world-in-c.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : hello-world-in-c.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int bruh();
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	bruh();
+	return EXIT_SUCCESS;
+}
---
+++ my-hello-world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int bruh(){
+	printf("Hello World!");
+	return 0;
+}
